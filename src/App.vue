c8a58252b10f01ee851a6f175cbcd9418927c897
Igor Besel <besel.igor@gmail.com>
2020-10-19 22:36:38 +0200

    Add german quodes

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,8 @@
       <div
         class="flex flex-col justify-center justify-self-stretch items-center text-center text-2xl xl:text-6xl font-bold"
       >
-        <div class="font-display p-8 lg:p-16 xl:p-24 my-2 mx-8 lg:mx-64 xl:mx-96">
-          VERSUCHE WENIGER PLASTIK ZU KONSUMIEREN UND SUCHE NACH PASSENDEN WEGEN,
-          DIE ZU DIR PASSEN.
+        <div class="uppercase font-display p-8 lg:p-16 xl:p-24 my-2 mx-8 lg:mx-64 xl:mx-96">
+          {{quote}}
         </div>
 
         <div class="pb-16 mb-16">
@@ -22,7 +21,7 @@
 
       <footer class="flex flex-col justify-center text-center items-center">
         <div class="">
-          <span class="font-body">DEINE TÄGLICHE GUTE TAT FÜR THE GOOD CHALLENGE IM NOVEMBER 2020</span> - <span class="font-serif">Mach mit & lass dich inspirieren</span>!
+          <span class="font-body">DEINE TÄGLICHE GUTE TAT FÜR THE GOOD CHALLENGE IM NOVEMBER 2020</span> - <span class="font-serif">Mach mit und lass dich inspirieren</span>!
         </div>
         <div class="flex flex-row text-3xl">
           <a href="https://www.facebook.com/GoDoGood2020/" target="_blank">
@@ -42,17 +41,21 @@
 
 <script lang="ts">
 import { ref } from 'vue'
+import quotes from '/lang/de.json'
 
 export default {
   setup() {
-    const background = ref(`bg-circle-${Math.floor(Math.random() * 16) + 1}`)
+    const background = ref<string>(`bg-circle-${Math.floor(Math.random() * 16) + 1}`)
+    const quote = ref<string>(quotes[Math.floor(Math.random() * quotes.length)])
 
     function toggle() {
       background.value = `bg-circle-${Math.floor(Math.random() * 16) + 1}`
+      quote.value = quotes[Math.floor(Math.random() * quotes.length)]
     }
 
     return {
       background,
+      quote,
       toggle,
     }
   },
